a3137c8fac4a812699a4efd49a4abeec2d867a41
Your Name <bsbawlia@gmail.com>
2023-07-19 13:29:35 +0530

    Dark Mode Added Successfully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import Textarea from './components/Textarea';
+// import Textarea from './components/Textarea';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar title="Birender's TextUtils" />
-      <Textarea heading="Enter text here to analyze" />
+      {/* <Textarea heading="Enter text here to analyze" /> */}
+      <About />
     </>
 
   );
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -20,22 +20,40 @@ export default function Textarea(props) {
         setText(event.target.value)
     }
 
+    let handleClearText = () => {
+        let newtext = " ";
+        setText(newtext);
+    }
+
+    let handleReverseText = () => {
+        let splitStr = text.split(" ");
+        splitStr = splitStr.reverse();
+        let newStr = splitStr.join(" ");
+        setText(newStr)
+    }
+
+
+
     return (
         <>
             <div className="container my-3 ">
                 <h1>{props.heading}</h1>
                 <label htmlFor="myText" className="form-label my-3">Example textarea</label>
-                <textarea className="form-control" id="myText" onChange={handleOnChange} value={text} rows="8"></textarea>
+                <textarea className="form-control" id="myText" onChange={handleOnChange} value={text} rows="6"></textarea>
 
                 <input className="btn btn-primary mx-1 my-3" type="submit" onClick={handleUpCase} value="Convert to Upper Case" />
 
                 <input className="btn btn-primary mx-1 my-3" type="submit" onClick={handleLowCase} value="Convert to Lower Case" />
+
+                <input className="btn btn-primary mx-1 my-3" type="submit" onClick={handleClearText} value="Convert to Clear text" />
+
+                <input className="btn btn-primary mx-1 my-3" type="submit" onClick={handleReverseText} value="Convert to Inverse Case" />
             </div>
 
             <div className="container">
                 <h1>Your Text Summary</h1>
                 <p>{text.split(" ").length} Words and {text.length} Word Count</p>
-                <h3>{text.split(" ").length * 0.01} Minutes Reading</h3>
+                <h6>{text.split(" ").length * 0.01} Minutes Reading</h6>
             </div>
 
             <div className="container">
